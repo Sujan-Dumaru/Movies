01b6dee990cde2b6c9dd9c9821a16e31a9b858f1
Sujan_Dumaru <sujan.dumaru@azrachit.com>
2023-09-11 14:13:42 +0545

    Initial setup for movie repository

---
+++ Movies.Application/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace Movies.Application.Models;
+
+public class Movie
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public int YearOfRelease { get; set; }
+    public List<string> Genres { get; init; } = new List<string>();
+}
---
+++ Movies.Application/Repositories/MovieRepository.cs
@@ -0,0 +1,32 @@
+﻿using Movies.Application.IRepositories;
+using Movies.Application.Models;
+
+namespace Movies.Application.Repositories;
+
+public class MovieRepository : IMovieRepository
+{
+    public Task<bool> CreateAsync(Movie movie)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Movie>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Movie?> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(Movie movie)
+    {
+        throw new NotImplementedException();
+    }
+}
