81ee41be6f77d5a657857faa7ae2d658900ad018
Sujan_Dumaru <sujan.dumaru@azrachit.com>
2023-09-11 09:57:48 +0545

    Defining the movie API contracts

---
+++ Movies.Contracts/Responses/MovieResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Contracts.Responses;
+
+public class MovieResponse
+{
+    [Required]
+    public Guid Id { get; init; }
+    public string Title { get; init; }
+    [Required]
+    public int YearOfRelease { get; init; }
+    [Required]
+    public IEnumerable<string> Genres { get; init; } = Enumerable.Empty<string>();
+}
